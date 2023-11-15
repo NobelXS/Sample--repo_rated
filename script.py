d8023b4ece715510bd7996efa75be0b16f844476
Nobel Siwela <xniwela@gmail.com>
2023-11-15 06:43:39 +0200

    Initial commit

---
+++ script.py
@@ -0,0 +1,13 @@
+import sys
+import requests
+# print(sys.version)
+print(sys.executable)
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+# print(greet('World'))
+# print(greet('Nobel'))
+r = requests.get('https://www.codecademy.com')
+print(r.status_code)
