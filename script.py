cbccf42a0b340a71978d6e1d27496f611864d95d
Nobel Siwela <xniwela@gmail.com>
2023-11-17 14:48:00 +0200

    Latest changes to the repo

--- script.py
+++ script.py
@@ -7,7 +7,9 @@ def greet(who_to_greet):
     greeting = 'Hello, {}'.format(who_to_greet)
     return greeting
 
-# print(greet('World'))
-# print(greet('Nobel'))
+print(greet('World'))
+print(greet('Nobel'))
+
 r = requests.get('https://www.codecademy.com')
 print(r.status_code)
+print(r.ok)
