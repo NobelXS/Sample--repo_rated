b4778a27060b926cba490949d3d4f41e482d5f53
Nobel Siwela <xniwela@gmail.com>
2023-11-17 14:56:29 +0200

    More changes

--- py_caret_tut.py
+++ py_caret_tut.py
@@ -1,4 +1,16 @@
 import pandas as pd
+from pycaret.classification import *
 
 df = pd.read_csv('CaseStudyData.csv')
-print(df.head(10))
+# print(df.head(10))
+
+s = setup(df, target='TargetVariable')
+
+best = compare_models()
+
+print(best)
+
+finalize_model(best)
+evaluate_model(best)
+predict_model(best)
+save_model('lr', 'lr_model_v1')
